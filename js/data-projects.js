a60bab7bb9f78e918492cbdc5ed701e89de0a9fa
avirahmandev <avirurrahnman@gmail.com>
2021-02-05 22:04:10 +0700

    menambahkan project game, mengubah asset gif ke webm, menambahkan google analytics, mengganti beberapa resource lokal ke cdn

--- js/data-projects.js
+++ js/data-projects.js
@@ -1,7 +1,19 @@
 var projects = [
 
 	{
-		isLive: true,
+		haveLivePrev: true,
+		haveSourceCode: false,
+		haveVideoPrev: true,
+		title: "Cube Platformer 2D",
+		description:"2D basic platformer game made with Godot Engine",
+		thumbnail: "assets/video/Preview-cube-platformer.webm",
+		githubLink: "javascript:void(0)",
+		liveUrl: "https://drive.google.com/file/d/1Cz-1Xf6VoCl4sZr9rffjgIDBPZhGgoiG/view?usp=sharing"
+	},
+	{
+		haveLivePrev: true,
+		haveSourceCode: true,
+		haveVideoPrev: false,
 		title: "TODO App",
 		description:"TODO APP is made with html, css, javascript and data is stored in html5 localStorage so that the data remains even though the app is closed.",
 		thumbnail: "assets/img/todo-app-thumbnail.png",
@@ -9,10 +21,12 @@ var projects = [
 		liveUrl: "https://avirahmandev.github.io/todo-app/"
 	},
 	{
-		isLive: false,
+		haveLivePrev: false,
+		haveSourceCode: true,
+		haveVideoPrev: true,
 		title: "Walking LED",
 		description: "A simple walking LED using Arduino, written in C++",
-		thumbnail: "assets/img/walking-led-thumbnail.gif",
+		thumbnail: "assets/video/Preview-walking-led.webm",
 		githubLink: "https://github.com/avirahmandev/walking-led",
 		liveUrl: "javascript:void(0)"
 	}
@@ -26,49 +40,79 @@ function generateProjects() {
 
 	for (let i=0; i < projects.length; i++) {
 
-		if (projects[i].isLive) {
-
-			itemProject += `
-				<div class="project-card">
-					<div class="thumbnail">
-						<img src="${projects[i].thumbnail}">
-					</div>
-					<div class="project-desc-box">
-						<h2 class="title-project">${projects[i].title}</h2>
-						<div class="desc-project">
-							${projects[i].description}
-						</div>
-					</div>
-					<br>
-					<div class="button-card">
-						<a href="${projects[i].liveUrl}"><i class="fas fa-link"></i> VISIT</a>
-						<a href="${projects[i].githubLink}" target="_blank"><i class="fab fa-github"></i> SOURCE</a>
-					</div>
+		// template card
+		var headWithVideo = `
+			<div class="project-card">
+				<div class="thumbnail">
+					<video autoplay loop muted>
+						<source src="${projects[i].thumbnail}" type="video/webm">
+						Your browser doesn't support the video tag.
+					</video>
 				</div>
 			`;
+
+		var headWithImg = `
+			<div class="project-card">
+				<div class="thumbnail">
+					<img src="${projects[i].thumbnail}">
+				</div>
+			`;
+
+		var bodySection = `
+					<div class="project-desc-box">
+							<h2 class="title-project">${projects[i].title}</h2>
+							<div class="desc-project">
+								${projects[i].description}
+							</div>
+					</div>
+					<br>
+				`;
+
+		var footerNormal = `
+				<div class="button-card">
+					<a href="${projects[i].liveUrl}"><i class="fas fa-link"></i> VISIT</a>
+					<a href="${projects[i].githubLink}" target="_blank"><i class="fab fa-github"></i> SOURCE</a>
+				</div>
+			</div>
+			`;
+
+		var footerBtn1Disabled = `
+				<div class="button-card">
+					<a href="${projects[i].liveUrl}" style="opacity: 0.5;"><i class="fas fa-link"></i> VISIT</a>
+					<a href="${projects[i].githubLink}" target="_blank"><i class="fab fa-github"></i> SOURCE</a>
+				</div>
+			</div>
+			`;
+
+		var footerBtn2Disabled = `
+				<div class="button-card">
+					<a href="${projects[i].liveUrl}"><i class="fas fa-link"></i> VISIT</a>
+					<a href="${projects[i].githubLink}" style="opacity: 0.5;"><i class="fab fa-github"></i> SOURCE</a>
+				</div>
+			</div>
+			`;
+
+		// cek apakah thumbnail video/gambar
+		if (projects[i].haveVideoPrev) {
+			itemProject += headWithVideo;
 		} else {
-
-			itemProject += `
-				<div class="project-card">
-					<div class="thumbnail">
-						<img src="${projects[i].thumbnail}">
-					</div>
-					<div class="project-desc-box">
-						<h2 class="title-project">${projects[i].title}</h2>
-						<div class="desc-project">
-							${projects[i].description}
-						</div>
-					</div>
-					<br>
-					<div class="button-card">
-						<a href="${projects[i].liveUrl}" style="opacity:0.5;"><i class="fas fa-link"></i> VISIT</a>
-						<a href="${projects[i].githubLink}" target="_blank"><i class="fab fa-github"></i> SOURCE</a>
-					</div>
-				</div>
-			`;
+			itemProject += headWithImg;
 		}
-		
-		
+
+		itemProject += bodySection;
+
+		// cek apakah punya live preview & akses source code
+		if (!projects[i].haveLivePrev && projects[i].haveSourceCode) {
+
+			itemProject += footerBtn1Disabled;
+		} else if (projects[i].haveLivePrev && !projects[i].haveSourceCode) {
+			
+			itemProject += footerBtn2Disabled;
+		} else {
+			
+			itemProject += footerNormal;
+		}
+
 
 		$(".project-items").html(itemProject);
 	}
